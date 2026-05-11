55201c218763f529f9934874d4f86d5cefa3d797
NeoByte991 <syedaftab6042@gmail.com>
2026-05-11 20:27:23 +0530

    Use sys.path.insert(0, ...) to ensure src is found first

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import altair as alt
 import pandas as pd
